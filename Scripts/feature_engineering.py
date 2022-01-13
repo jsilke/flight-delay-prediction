08bb502f6dd94b3ba9da70654f69398669ce6351
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2022-01-12 20:36:19 -0500

    Update documentation for clarity and fix typos. Add a higher level function that converts multiple ordinal features into one dataframe.

--- Scripts/feature_engineering.py
+++ Scripts/feature_engineering.py
@@ -1,46 +1,84 @@
 import pandas as pd
 
 
-def rank_column_by_metric(df_with_rank_by_and_to_rank: pd.DataFrame,
-                          column_to_rank: str,
-                          rank_by_column: str,
-                          method: str = 'dense',
-                          ) -> tuple[pd.Series, dict]:
+def rank_column_by_mean(df: pd.DataFrame,
+                        to_rank_name: str,
+                        rank_by_name: str,
+                        method: str = 'dense',
+                        ) -> tuple[pd.Series, dict]:
     """
-    Converts categorical columns into ordinal columns using the pandas groupby and transform categorical methods.
+    Converts categorical columns into ordinal columns using the pandas `groupby`, `rank` and `transform` categorical methods.
     Because this is a wrapper function, limitations stem from those of its subcomponents.
 
     Parameters:
 
-        `df_with_rank_by_and_to_rank`: pd.DataFrame - Input DataFrame that is expected to contain both the column
-            to be ranked as well as the column to use for ranking values.
+        `df`: pd.DataFrame - Input DataFrame that is expected to contain both the categorical column (`to_rank_name`) to be 
+            ranked as well as the numeric column (`rank_by_name`) to use for ranking values.
 
-        `column_to_rank`: str - The name of the categorical column to be ranked as a string. Must be a column in the
-            input DataFrame.
+        `to_rank_name`: str - The name of the categorical column to be ranked as a string. Must be a column in the
+            input DataFrame (`df`).
 
-        `rank_by_column`: str - The name of the numeric column to be used for ranking the categorical column. Must be 
-            a column in the input DataFrame.
+        `rank_by_name`: str - The name of the numeric column to be used for ranking the categorical column. Must be 
+            a column in the input DataFrame (`df`).
 
-        `metric`: str = 'mean' - The metric (function name) to be broadcasted over the numeric column to establish rankings 
-            by the pandas `transform` method. Defaults to 'mean'.
-
-        `method`: str = 'dense' - the method to be used by the pandas rank method to determine ranking. Our default of 
-            'dense' is sensible, but {‘average’, ‘min’, ‘max’, ‘first’, ‘dense’} are all accepted here.
+        `method`: str = 'dense' - the method to be used by the pandas `rank` method to determine ranking. Our default of 
+            'dense' is sensible, but {'average', 'min', 'max', 'first', 'dense'} are all accepted here.
 
     Returns:
+
         tuple[pd.Series, dict] - A tuple with the transformed series and a dictionary containing the mappings to be applied
             on the test data.
+
     """
-    _metric_series = df_with_rank_by_and_to_rank.groupby(
-        column_to_rank)[rank_by_column].transform('mean')
+    _metric_series = df.groupby(to_rank_name)[rank_by_name].transform('mean')
     ranked_series = _metric_series.rank(method=method)
 
-    _mapping = df_with_rank_by_and_to_rank.groupby(
-        column_to_rank)[rank_by_column].mean().rank()
+    _mapping = df.groupby(to_rank_name)[rank_by_name].mean().rank()
     rank_mapping = {index: rank for index,
                     rank in zip(_mapping.index, _mapping)}
 
     return ranked_series, rank_mapping
+
+
+def rank_features_by_mean(df: pd.DataFrame,
+                          to_rank_names: list,
+                          rank_by_name: str,
+                          method: str = 'dense',
+                          ) -> tuple[pd.DataFrame, dict[dict]]:
+    """
+    Applies `rank_column_by_mean` on a list of categorical features (expected subset of `df`.columns) to generate a pandas
+    DataFrame of ranked columns as well as a dict of their mappings.
+
+    Parameters:
+
+        `df`: pd.DataFrame - Input DataFrame that is expected to contain both the categorical column (`to_rank_name`) to be 
+            ranked as well as the numeric column (`rank_by_name`) to use for ranking values.
+
+        `to_rank_names`: list - The list of names of the categorical columns to be ranked. Expected be columns in the
+            input DataFrame (`df`).
+
+        `rank_by_name`: str - The name of the numeric column to be used for ranking the categorical column. Must be 
+            a column in the input DataFrame (`df`).
+
+        `method`: str = 'dense' - the method to be used by the pandas `rank` method to determine ranking. Our default of 
+            'dense' is sensible, but {'average', 'min', 'max', 'first', 'dense'} are all accepted here.
+
+    Returns:
+
+        tuple[pd.DataFrame, dict[dict]] - A tuple with the transformed DataFrame and a dictionary containing the mappings 
+            to be applied on the test data.
+
+    """
+    ranked_features = {}
+    mappings = {}
+
+    for to_rank_name in to_rank_names:
+        ranked_features[f'{to_rank_name}_ranked'], mappings[to_rank_name] = rank_column_by_mean(df,
+                                                                                                to_rank_name,
+                                                                                                rank_by_name,
+                                                                                                method)
+
+    return pd.DataFrame(ranked_features), mappings
 
 
 def main():
